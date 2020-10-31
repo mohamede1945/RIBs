c9e93f0650f9f57f19c7b261bbecffaf03260e34
Mohamed Afifi <maafifi@amazon.com>
2020-10-31 16:21:10 -0400

    iOS 9 as min supported target

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RIBs",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
     ],
     products: [
         .library(name: "RIBs", targets: ["RIBs"]),
